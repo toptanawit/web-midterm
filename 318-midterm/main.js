b1c30c35ebe7c566f19e8fd050bcf504bd089c82
toptanawit <tanavitptop@gmail.com>
2022-11-03 19:44:36 +0700

    add users database

--- 318-midterm/main.js
+++ 318-midterm/main.js
@@ -5,39 +5,76 @@ var bodyParser = require('body-parser');
 app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 const HandyStorage = require('handy-storage');
+var mysql = require('mysql');
+const session = require('express-session');
  
+app.set('views', path.join(__dirname+'/public', 'views'));
+app.set('view engine', 'ejs');
+
 const storage = new HandyStorage({
     beautify: true
 });
 
 storage.connect('./message.json');
 
-const session = require('express-session');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'midterm'
+});
+
 app.use(session({
     secret: 'COSCI', cookie: { maxAge: 60000000 },
     resave: true, saveUninitialized: false
 }));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get('/login', (req,res)=>{
     res.sendFile(path.join(__dirname, "/public/login.html"));  
 });
 
+app.get('/register', (req,res)=>{
+    res.sendFile(path.join(__dirname, "/public/register.html"));  
+});
+
+app.post('/register', (req, res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    const post = {
+        username: username,
+        password: password,
+    };
+    connection.query('insert into users set ?', post, (err)=>{
+        console.log('Registered Successfully');
+    });
+    res.redirect('/login');
+});
+
 app.post('/authen', (req, res)=>{
-    const _username = req.body.username;
-    const _password = req.body.password;
+    var username = req.body.username;
+    var password = req.body.password;
 
     if (req.session.isLoggedIn != null && req.session.isLoggedIn == true){
         res.redirect("/academic");
     }
 
-    if (req.body.username == "admin" && req.body.password == "Web999"){
-        req.session.username = req.query.username;
-        req.session.isLoggedIn = true;
-        res.redirect("/academic");
-    } else if (req.body.username == "commu" && req.body.password == "Cosci7749") {
-        req.session.username = req.query.username;
-        req.session.isLoggedIn = true;
-        res.redirect("/academic");
+    if (username && password) {
+        connection.query(
+            "select * from users where username = ? and password = ?",
+            [username, password],
+            (error, results, fields)=>{
+                if (results.length > 0) {
+                    req.session.isLoggedIn = true;
+                    req.session.username = username;
+                    res.redirect('/academic');
+                } else {
+                    res.sendFile(path.join(__dirname, "/public/relogin.html"));
+                }
+            }
+        );
     } else {
         res.sendFile(path.join(__dirname, "/public/relogin.html"));
     }
@@ -58,6 +95,62 @@ app.get('/read', function(req, res){
       res.redirect("/login");
     }  
 });
+
+app.get('/dbread', function(req, res){
+    if (req.session.isLoggedIn != null && req.session.isLoggedIn == true){
+        connection.query('select * from users', (err, result)=>{
+            res.render('dbread.ejs', {
+                posts: result
+            });
+        }); 
+    }else{
+      res.redirect("/login");
+    }  
+});
+
+app.get('/edit/:id', (req, res)=>{
+    const edit_postID = req.params.id;
+
+    connection.query(
+        'select * from users where id = ?',
+        [edit_postID],
+        (err, results)=>{
+            if (results) {
+                res.render('dbedit.ejs', {
+                    post: results[0]
+                });
+            }
+        }
+    );
+});
+
+app.post('/edit/:id', (req, res)=>{
+    const update_username = req.body.username;
+    const update_password = req.body.password;
+    const id = req.params.id;
+
+    connection.query(
+        'update users set username = ?, password = ? where id = ?',
+        [update_username, update_password, id],
+        (err, results)=>{
+            if (results.changedRows === 1) {
+                console.log('Post Updated');
+            }
+            return res.redirect('/dbread');
+        }
+    );
+});
+
+app.get('/delete/:id', (req, res)=>{
+    connection.query(
+        'delete from users where id = ?',
+        [req.params.id],
+        (err, results)=>{
+            return res.redirect('/dbread');
+        }
+    );
+});
+
 
 app.get('/logout', function(req, res){
     req.session.destroy();
